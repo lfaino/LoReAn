748cf6ab71835c4f1d7afaa9ed53fbec359ab50a
lfaino <luigi.faino@wur.nl>
2017-03-31 15:19:27 +0200

    v0.2

--- third_party/conf_files/createUser.sh
+++ third_party/conf_files/createUser.sh
@@ -6,4 +6,5 @@ cat /home/$name/.bashrc /etc/profile.d/pathToExport.sh  > /home/$name/.bashrc_ne
 usermod -d /var/lib/mysql/ mysql
 /etc/init.d/mysql start
 mysql --user="root" --password="lorean" --execute="set global sql_mode='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';"
+chown -R $name:$name /home/$name
 su $name
