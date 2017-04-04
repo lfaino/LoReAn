0c8094bb3220ef8daa5ec21117932a379dbc712b
lfaino <luigi.faino@wur.nl>
2017-04-04 08:20:01 +0200

    v0.2

--- third_party/conf_files/createUser.sh
+++ third_party/conf_files/createUser.sh
@@ -7,4 +7,5 @@ usermod -d /var/lib/mysql/ mysql
 /etc/init.d/mysql start
 mysql --user="root" --password="lorean" --execute="set global sql_mode='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';"
 chown -R $name:$name /home/$name
+chown -R $name:$name /opt/LoReAn/
 su $name
