c34eebc0182903a0aef3abfae62f3be40cf3edb9
lfaino <luigi.faino@gmail.com>
2017-08-02 14:57:50 +0200

    v0.3

--- third_party/conf_files/createUser.sh
+++ third_party/conf_files/createUser.sh
@@ -9,6 +9,7 @@ usermod -d /var/lib/mysql/ mysql
 mysql --user="root" --password="lorean" --execute="set global sql_mode='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';"
 chown -R $name:$name /home/$name/.gm_key
 chown -R $name:$name /home/$name/.bashrc
+cp -r /opt/LoReAn/third_party/software/augustus/ /home/$name/
 #chown -R $name:$name /opt/LoReAn/
 chmod -R 775 /opt/LoReAn
 su $name
