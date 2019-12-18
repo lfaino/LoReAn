6b1bea4d27b9041b0fc67b52d49e104fb8741e8e
lfaino <M3Fw3K2h>
2019-12-18 09:26:29 +0100

    V2.0

--- code/test_transform.py
+++
@@ -1,9 +0,0 @@
-from collections import namedtuple
-
-MemInfoEntry = namedtuple('MemInfoEntry', ['value', 'unit'])
-meminfo = {}
-with open('/proc/meminfo') as file:
-    for line in file:
-        key, value, *unit = line.strip().split()
-        meminfo[key.rstrip(':')] = MemInfoEntry(value, unit)
-memtotal = round((int(meminfo['MemTotal'].value) / 10000000) - 2)
